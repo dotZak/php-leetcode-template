02ba8b6550712946b09656e0e8a3508d6ccaddfd
Zak Knudsen <7200184+dotZak@users.noreply.github.com>
2024-06-13 12:10:23 +0200

    Added basic files.

---
+++ tests/SolutionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SolutionTest extends TestCase
+{
+    public function testMultipleCustomersCanBeTheWealthiest(): void
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
